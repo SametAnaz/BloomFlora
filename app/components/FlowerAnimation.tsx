94b44ecc6ad353783365e21bd119dce3bcf15182
Samet <sametanaz.tr@gmail.com>
2026-01-24 23:34:04 +0300

    Performance optimization

--- app/components/FlowerAnimation.tsx
+++ app/components/FlowerAnimation.tsx
@@ -27,21 +27,14 @@ export default function FlowerAnimation() {
             <div className="flower__white-circle"></div>
 
             <div className="flower__light flower__light--1"></div>
-            <div className="flower__light flower__light--2"></div>
             <div className="flower__light flower__light--3"></div>
-            <div className="flower__light flower__light--4"></div>
             <div className="flower__light flower__light--5"></div>
-            <div className="flower__light flower__light--6"></div>
-            <div className="flower__light flower__light--7"></div>
-            <div className="flower__light flower__light--8"></div>
           </div>
           <div className="flower__line">
             <div className="flower__line__leaf flower__line__leaf--1"></div>
             <div className="flower__line__leaf flower__line__leaf--2"></div>
             <div className="flower__line__leaf flower__line__leaf--3"></div>
             <div className="flower__line__leaf flower__line__leaf--4"></div>
-            <div className="flower__line__leaf flower__line__leaf--5"></div>
-            <div className="flower__line__leaf flower__line__leaf--6"></div>
           </div>
         </div>
 
@@ -53,20 +46,13 @@ export default function FlowerAnimation() {
             <div className="flower__leaf flower__leaf--4"></div>
             <div className="flower__white-circle"></div>
 
-            <div className="flower__light flower__light--1"></div>
             <div className="flower__light flower__light--2"></div>
-            <div className="flower__light flower__light--3"></div>
             <div className="flower__light flower__light--4"></div>
-            <div className="flower__light flower__light--5"></div>
             <div className="flower__light flower__light--6"></div>
-            <div className="flower__light flower__light--7"></div>
-            <div className="flower__light flower__light--8"></div>
           </div>
           <div className="flower__line">
             <div className="flower__line__leaf flower__line__leaf--1"></div>
-            <div className="flower__line__leaf flower__line__leaf--2"></div>
             <div className="flower__line__leaf flower__line__leaf--3"></div>
-            <div className="flower__line__leaf flower__line__leaf--4"></div>
           </div>
         </div>
 
@@ -79,18 +65,11 @@ export default function FlowerAnimation() {
             <div className="flower__white-circle"></div>
 
             <div className="flower__light flower__light--1"></div>
-            <div className="flower__light flower__light--2"></div>
-            <div className="flower__light flower__light--3"></div>
             <div className="flower__light flower__light--4"></div>
-            <div className="flower__light flower__light--5"></div>
-            <div className="flower__light flower__light--6"></div>
             <div className="flower__light flower__light--7"></div>
-            <div className="flower__light flower__light--8"></div>
           </div>
           <div className="flower__line">
-            <div className="flower__line__leaf flower__line__leaf--1"></div>
             <div className="flower__line__leaf flower__line__leaf--2"></div>
-            <div className="flower__line__leaf flower__line__leaf--3"></div>
             <div className="flower__line__leaf flower__line__leaf--4"></div>
           </div>
         </div>
@@ -106,31 +85,10 @@ export default function FlowerAnimation() {
           <div className="flower__grass flower__grass--1">
             <div className="flower__grass--top"></div>
             <div className="flower__grass--bottom"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--1"></div>
             <div className="flower__grass__leaf flower__grass__leaf--2"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--3"></div>
             <div className="flower__grass__leaf flower__grass__leaf--4"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--5"></div>
             <div className="flower__grass__leaf flower__grass__leaf--6"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--7"></div>
             <div className="flower__grass__leaf flower__grass__leaf--8"></div>
-            <div className="flower__grass__overlay"></div>
-          </div>
-        </div>
-
-        <div className="growing-grass">
-          <div className="flower__grass flower__grass--2">
-            <div className="flower__grass--top"></div>
-            <div className="flower__grass--bottom"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--1"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--2"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--3"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--4"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--5"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--6"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--7"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--8"></div>
-            <div className="flower__grass__overlay"></div>
           </div>
         </div>
 
@@ -141,35 +99,17 @@ export default function FlowerAnimation() {
         </div>
 
         <div className="grow-ans" style={{ '--d': '2.8s' } as React.CSSProperties}>
-          <div className="flower__g-right flower__g-right--2">
-            <div className="leaf"></div>
-          </div>
-        </div>
-
-        <div className="grow-ans" style={{ '--d': '2.8s' } as React.CSSProperties}>
           <div className="flower__g-front">
             <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--1">
-              <div className="flower__g-front__leaf"></div>
-            </div>
-            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--2">
               <div className="flower__g-front__leaf"></div>
             </div>
             <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--3">
               <div className="flower__g-front__leaf"></div>
             </div>
-            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--4">
-              <div className="flower__g-front__leaf"></div>
-            </div>
             <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--5">
               <div className="flower__g-front__leaf"></div>
             </div>
-            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--6">
-              <div className="flower__g-front__leaf"></div>
-            </div>
             <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--7">
-              <div className="flower__g-front__leaf"></div>
-            </div>
-            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--8">
               <div className="flower__g-front__leaf"></div>
             </div>
             <div className="flower__g-front__line"></div>
@@ -180,28 +120,19 @@ export default function FlowerAnimation() {
           <div className="flower__g-fr">
             <div className="leaf"></div>
             <div className="flower__g-fr__leaf flower__g-fr__leaf--1"></div>
-            <div className="flower__g-fr__leaf flower__g-fr__leaf--2"></div>
             <div className="flower__g-fr__leaf flower__g-fr__leaf--3"></div>
-            <div className="flower__g-fr__leaf flower__g-fr__leaf--4"></div>
             <div className="flower__g-fr__leaf flower__g-fr__leaf--5"></div>
-            <div className="flower__g-fr__leaf flower__g-fr__leaf--6"></div>
             <div className="flower__g-fr__leaf flower__g-fr__leaf--7"></div>
-            <div className="flower__g-fr__leaf flower__g-fr__leaf--8"></div>
           </div>
         </div>
 
+        {/* Optimized long grass - reduced from 8 to 4 */}
         <div className="long-g long-g--0">
           <div className="grow-ans" style={{ '--d': '3s' } as React.CSSProperties}>
             <div className="leaf leaf--0"></div>
           </div>
-          <div className="grow-ans" style={{ '--d': '2.2s' } as React.CSSProperties}>
-            <div className="leaf leaf--1"></div>
-          </div>
           <div className="grow-ans" style={{ '--d': '3.4s' } as React.CSSProperties}>
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '3.6s' } as React.CSSProperties}>
-            <div className="leaf leaf--3"></div>
+            <div className="leaf leaf--1"></div>
           </div>
         </div>
 
@@ -209,59 +140,8 @@ export default function FlowerAnimation() {
           <div className="grow-ans" style={{ '--d': '3.6s' } as React.CSSProperties}>
             <div className="leaf leaf--0"></div>
           </div>
-          <div className="grow-ans" style={{ '--d': '3.8s' } as React.CSSProperties}>
-            <div className="leaf leaf--1"></div>
-          </div>
           <div className="grow-ans" style={{ '--d': '4s' } as React.CSSProperties}>
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '4.2s' } as React.CSSProperties}>
-            <div className="leaf leaf--3"></div>
-          </div>
-        </div>
-
-        <div className="long-g long-g--2">
-          <div className="grow-ans" style={{ '--d': '4s' } as React.CSSProperties}>
-            <div className="leaf leaf--0"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '4.2s' } as React.CSSProperties}>
             <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '4.4s' } as React.CSSProperties}>
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '4.6s' } as React.CSSProperties}>
-            <div className="leaf leaf--3"></div>
-          </div>
-        </div>
-
-        <div className="long-g long-g--3">
-          <div className="grow-ans" style={{ '--d': '4s' } as React.CSSProperties}>
-            <div className="leaf leaf--0"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '4.2s' } as React.CSSProperties}>
-            <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '3s' } as React.CSSProperties}>
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '3.6s' } as React.CSSProperties}>
-            <div className="leaf leaf--3"></div>
-          </div>
-        </div>
-
-        <div className="long-g long-g--4">
-          <div className="grow-ans" style={{ '--d': '4s' } as React.CSSProperties}>
-            <div className="leaf leaf--0"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '4.2s' } as React.CSSProperties}>
-            <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '3s' } as React.CSSProperties}>
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '3.6s' } as React.CSSProperties}>
-            <div className="leaf leaf--3"></div>
           </div>
         </div>
 
@@ -269,44 +149,17 @@ export default function FlowerAnimation() {
           <div className="grow-ans" style={{ '--d': '4s' } as React.CSSProperties}>
             <div className="leaf leaf--0"></div>
           </div>
-          <div className="grow-ans" style={{ '--d': '4.2s' } as React.CSSProperties}>
-            <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '3s' } as React.CSSProperties}>
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '3.6s' } as React.CSSProperties}>
-            <div className="leaf leaf--3"></div>
-          </div>
-        </div>
-
-        <div className="long-g long-g--6">
-          <div className="grow-ans" style={{ '--d': '4.2s' } as React.CSSProperties}>
-            <div className="leaf leaf--0"></div>
-          </div>
           <div className="grow-ans" style={{ '--d': '4.4s' } as React.CSSProperties}>
             <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '4.6s' } as React.CSSProperties}>
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '4.8s' } as React.CSSProperties}>
-            <div className="leaf leaf--3"></div>
           </div>
         </div>
 
         <div className="long-g long-g--7">
-          <div className="grow-ans" style={{ '--d': '3s' } as React.CSSProperties}>
+          <div className="grow-ans" style={{ '--d': '3.2s' } as React.CSSProperties}>
             <div className="leaf leaf--0"></div>
           </div>
-          <div className="grow-ans" style={{ '--d': '3.2s' } as React.CSSProperties}>
-            <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans" style={{ '--d': '3.5s' } as React.CSSProperties}>
-            <div className="leaf leaf--2"></div>
-          </div>
           <div className="grow-ans" style={{ '--d': '3.6s' } as React.CSSProperties}>
-            <div className="leaf leaf--3"></div>
+            <div className="leaf leaf--1"></div>
           </div>
         </div>
       </div>
